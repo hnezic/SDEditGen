eaaa1ca2e2c3247661df54e938d9bbd9539cc655
Hrvoje Nezic <enezhrv@ericsson.com>
2018-11-14 14:46:03 +0100

    Finished the parser.

--- src/main/scala/io/enezhrv/sdedit/Main.scala
+++ src/main/scala/io/enezhrv/sdedit/Main.scala
@@ -4,7 +4,7 @@ import fastparse._, NoWhitespace._
 
 object Main extends App
 {
-    def program[_: P] : P[Program] = (title.? ~ objects ~ statements) . map { case (tit, obj, st) => Program(tit, obj, st) }
+    def program[_: P] : P[Program] = P( (title.? ~ objects ~ statements) . map { case (tit, obj, st) => Program(tit, obj, st) } )
 
     def objects[_: P] : P[List[ObjectDecl]] = P( (key("objects") ~ str("{") ~ objectDecl.rep(1) ~ str("}")).map(_.toList) )
     def objectDecl[_: P] : P[ObjectDecl] = P( (ident ~ ident ~ flag.rep.map(_.toSet))
@@ -13,16 +13,35 @@ object Main extends App
 
     def statements[_: P] : P[List[Statement]] = P( statement.rep.map(_.toList) )
     def statementsInBraces[_: P] : P[List[Statement]] = P( str("{") ~ statement.rep.map(_.toList) ~ str("}") )
-    def statement[_: P] : P[Statement] = P( diagramLink )
+    def statement[_: P] : P[Statement] = P( constructor | method | code | objectCreation | methodCall | diagramLink | fragment | loop | alt )
 
     // Method call, object creation
-    def arguments[_: P] : P[List[Argument]] = str("(") ~ argument.rep(sep=",").map(_.toList) ~ str(")")
-    def argument[_: P] : P[Argument] = ident . map(arg => Argument(arg))
-    def result[_: P] : P[Result] = key("return") ~ ident . map(r => Result(r))
+    def objectCreation[_: P] : P[ObjectCreation] = P( (key("create") ~ ident ~ arguments.? ~ result.? ~ optStatements)
+                                                   . map { case (obj, args, res, st) => new ObjectCreation(obj, args, res, st) } )
+    def methodCall[_: P]: P[MethodCall] = P( (key("call") ~ ident ~ methodName ~ arguments.? ~ result.? ~ optStatements)
+                                                   . map { case (obj, met, args, res, st) => new MethodCall(obj, met, args, res, st) } )
+    def methodName[_: P] : P[String] = P( ident | string )
+    def optStatements[_: P]: P[Option[List[Statement]]] = P( statementsInBraces.map(Option(_)) | optEnd.map (_ => None) )
+
+    def arguments[_: P] : P[List[Argument]] = P( str("(") ~ argument.rep(sep=",").map(_.toList) ~ str(")") )
+    def argument[_: P] : P[Argument] = P( ident . map(arg => Argument(arg)) )
+    def result[_: P] : P[Result] = P( key("return") ~ ident . map(r => Result(r)) )
 
     // Method, constructor, call
     def constructor[_: P] : P[Constructor] = P( (key("constructor") ~ ident ~ statementsInBraces) . map{ case (obj, st) => new Constructor(obj, st) } )
+    def method[_: P]: P[Method] = P( (key("method") ~ ident ~ methodName ~ statementsInBraces)
+                                      . map { case (obj, met, st) => new Method (obj, met, st) } )
     def code[_: P] : P[Code] = P( (key("code") ~ ident ~ statementsInBraces) . map{ case (obj, st) => new Code(obj, st) } )
+
+    // Fragments
+    def fragment[_: P] : P[Fragment] = P( (key("fragment") ~ ident.? ~ fragmentCommon) . map { case (type_, common) => new Fragment(type_, common) } )
+    def loop[_: P] : P[Loop] = P( key("loop") ~ fragmentCommon . map(new Loop(_)) )
+    def alt[_: P] : P[Alt] = P( key("alt") ~ fragmentCommon . map(new Alt(_)) )
+
+    def fragmentCommon[_: P] : P[FragmentCommon] = P( (string.? ~ fragmentItems) . map { case (text, items) => FragmentCommon(text, items) } )
+    def fragmentItems[_: P] : P[List[FragmentItem]] = P( str("{") ~ fragmentItem.rep(1).map(_.toList) ~ str("}") )
+    def fragmentItem[_: P] : P[FragmentItem] = P( statement | sectionDelimiter )
+    def sectionDelimiter[_: P] : P[FragmentItem] = P( key("section") ~ string.? . map(SectionDelimiter) )
 
     // Notes, comments
     def diagramLink[_: P] : P[DiagramLink] = P( (key("diagramLink") ~ string ~ ident.? ~ number ~ optEnd)
