c16c23ddc066186332c832cd1507098d3a7577f1
Hrvoje Nezic <enezhrv@ericsson.com>
2018-11-15 13:42:21 +0100

    Added tests.

---
+++ src/test/scala/io/enezhrv/sdedit/TitleTest.scala
@@ -0,0 +1,29 @@
+package io.enezhrv.sdedit
+
+class TitleTest extends UnitTest
+{
+    test("Optional title before object declarations") {
+        val input =
+            """
+              | title "Skills421 - BookingService"
+              | objects {
+              |     test AdapterTest named existing
+              |     adapter Adapter
+              |     manager Manager
+              |     context Context
+              | }
+            """.stripMargin
+
+        val expected =
+            """
+              |#![Skills421 - BookingService]
+              |test:AdapterTest
+              |/adapter:Adapter[a]
+              |/manager:Manager[a]
+              |/context:Context[a]
+              |
+            """.stripLines
+
+        parseAssert (input, expected)
+    }
+}
---
+++ src/test/scala/io/enezhrv/sdedit/WithoutStatements.scala
@@ -0,0 +1,27 @@
+package io.enezhrv.sdedit
+
+class WithoutStatements extends UnitTest
+{
+    test("Statements are not required") {
+        val input =
+            """
+              | objects {
+              |     test AdapterTest named existing
+              |     adapter Adapter
+              |     manager Manager
+              |     context Context
+              | }
+            """.stripMargin
+
+        val expected =
+            """
+              |test:AdapterTest
+              |/adapter:Adapter[a]
+              |/manager:Manager[a]
+              |/context:Context[a]
+              |
+            """.stripLines
+
+        parseAssert (input, expected)
+    }
+}
