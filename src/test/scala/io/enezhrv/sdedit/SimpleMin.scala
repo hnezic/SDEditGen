5fd848d7dbc3ca16924f57786504154dd0efa9db
Hrvoje Nezic <hrvoje.vg@gmail.com>
2019-04-25 14:17:16 +0200

    Extended SimpleMin test to handle 3 variants for initial entity: object, method and constructor.

--- src/test/scala/io/enezhrv/sdedit/SimpleMin.scala
+++ src/test/scala/io/enezhrv/sdedit/SimpleMin.scala
@@ -2,50 +2,61 @@ package io.enezhrv.sdedit
 
 class SimpleMin extends UnitTest
 {
-    test("Simple diagram (simple create and call statements)") {
-        val input =
-            """
-              | objects {
-              |     test: AdapterTest named existing
-              |     adapter: Adapter
-              |     manager: Manager
-              |     context: Context
-              | }
-              |
-              | object test {
-              |     create adapter
-              |
-              |     call adapter.init {
-              |         create manager {
-              |             create context
-              |         }
-              |     }
-              |
-              |     call manager.manage(arg1, arg2) {
-              |         loop {
-              |             call context.getItem
-              |         }
-              |     }
-              | }
-            """.stripMargin
+    def input(initialEntity: String) =
+        s"""
+          | objects {
+          |     test: AdapterTest named existing
+          |     adapter: Adapter
+          |     manager: Manager
+          |     context: Context
+          | }
+          |
+          | $initialEntity {
+          |     create adapter
+          |
+          |     call adapter.init {
+          |         create manager {
+          |             create context
+          |         }
+          |     }
+          |
+          |     call manager.manage(arg1, arg2) {
+          |         loop {
+          |             call context.getItem
+          |         }
+          |     }
+          | }
+        """.stripMargin
 
-        val expected =
-            """
-              |test:AdapterTest
-              |/adapter:Adapter[a]
-              |/manager:Manager[a]
-              |/context:Context[a]
-              |
-              |test:adapter.new
-              |test:adapter.init
-              |    adapter:manager.new
-              |        manager:context.new
-              |test:manager.manage(arg1, arg2)
-              |    [c:loop]
-              |        manager:context.getItem
-              |    [/c]
-            """.stripLines
+    val expected =
+        """
+          |test:AdapterTest
+          |/adapter:Adapter[a]
+          |/manager:Manager[a]
+          |/context:Context[a]
+          |
+          |test:adapter.new
+          |test:adapter.init
+          |    adapter:manager.new
+          |        manager:context.new
+          |test:manager.manage(arg1, arg2)
+          |    [c:loop]
+          |        manager:context.getItem
+          |    [/c]
+        """.stripLines
 
-        parseAssert (input, expected)
+    test("Simple diagram (object test)")
+    {
+        parseAssert (input("object test"), expected)
+    }
+
+    test("Simple diagram (method test testMethod)")
+    {
+        parseAssert (input("method test testMethod"), expected)
+    }
+
+    test("Simple diagram (constructor test)")
+    {
+        parseAssert (input("constructor test"), expected)
     }
 }
